808f86d5bc7a2eb86c0eac27fb1ce23bd28a5657
agosarias <agosarias99@gmail.com>
2025-07-23 18:19:52 +0200

    add ft_stackadd_back

--- util/ft_stack.c
+++ util/ft_stack.c
@@ -1,6 +1,6 @@
 #include "push_swap.h"
 
-t_stack	*ft_stacknew(void *content)
+t_stack	*ft_stacknew(int content)
 {
 	t_stack	*node;
 
@@ -11,4 +11,22 @@ t_stack	*ft_stacknew(void *content)
 	node->next = NULL;
     node->prev = NULL;
 	return (node);
+}
+
+void	ft_stackadd_back(t_stack **stack, t_stack *new)
+{
+	t_stack	*node;
+	
+	if(!new)
+		return;
+	if(!*stack)
+	{
+		*stack = new;
+	       return ;	
+	}
+	node = *stack;
+	while (node -> next != NULL)
+		node = node -> next;
+	node -> next = new;
+	new -> prev = node;
 }
